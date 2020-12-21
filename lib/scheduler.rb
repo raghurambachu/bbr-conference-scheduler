b79a89f7e737d28551008037382b237dac45b86b
Raghuram <1993raghuram@gmail.com>
2020-12-21 13:08:42 +0530

    Added methods to print scheduleable and non scheduleable talks.

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -3,56 +3,17 @@ require_relative("qualified_talks")
 require_relative("track")
 require_relative("morning_session")
 require_relative("afternoon_session")
+require_relative("conference_scheduler")
 
-class ConferenceScheduler
+track1 = Track.new("1st Track",MorningSession.new(Time.local(2020 , 12, 20, 9),Time.local(2020, 12, 20, 12),3),AfternoonSession.new(Time.local(2020, 12, 20, 13),Time.local(2020, 12, 20, 17),4))
+track2 = Track.new("2nd Track",MorningSession.new(Time.local(2020 , 12, 20, 9),Time.local(2020, 12, 20, 12),3),AfternoonSession.new(Time.local(2020, 12, 20, 13),Time.local(2020, 12, 20, 17),4))
 
-    include QualifiedTalks
-    attr_reader :all_talks
-    def initialize
-        @all_talks = QualifiedTalks::Talks
-        @not_scheduleable = []
-    end
+scheduler1 = ConferenceScheduler.new(track1,track2)
 
-    def add_talk_to_track(track,talk)
-        if !track.morning.full?(talk.duration)
-            track.add_talk(talk,true)
-        elsif !track.afternoon.full?(talk.duration)
-            track.add_talk(talk,false)
-        else
-            @not_scheduleable << talk
-        end
-    end
+# track1, track2, not_scheduleable = scheduler1.schedule
+
+scheduler1.print_scheduled_talks
+scheduler1.print_non_scheduled_talks
 
 
-    def schedule(track1,track2)
-        counter = 0
-        all_talks.sort! { |talk1,talk2| talk1.duration <=> talk2.duration  }  
-     
-        while !all_talks.empty?
-            talk = all_talks.first
-            if counter.even?
-               add_talk_to_track(track1,talk)
-            else
-                add_talk_to_track(track2,talk)
-            end
-            counter += 1
-            all_talks.shift
-        end
-        return [track1,track2,@not_scheduleable]
-    end
 
-end
-
-
-track1 = Track.new("1st Track",MorningSession.new(Time.local(2020 , 12, 20, 9),Time.local(2020, 12, 20, 12),3),AfternoonSession.new(Time.local(2020, 12, 20, 13),Time.local(2020, 12, 20, 17),3))
-track2 = Track.new("2nd Track",MorningSession.new(Time.local(2020 , 12, 20, 9),Time.local(2020, 12, 20, 12),3),AfternoonSession.new(Time.local(2020, 12, 20, 13),Time.local(2020, 12, 20, 17),3))
-
-scheduler1 = ConferenceScheduler.new
-
-track1, track2, not_scheduleable = scheduler1.schedule(track1,track2)
-
-p track2.morning.talks
-puts 
-p track1.morning.talks
-
-p not_scheduleable
